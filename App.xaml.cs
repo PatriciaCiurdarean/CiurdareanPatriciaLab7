bc23d055cfb6c1231ccfdce65e9fa10a88377d64
PATRICIA CIURDĂREAN <patricia.ciurdarean@stud.ubbcluj.ro>
2025-11-26 15:45:06 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace CiurdareanPatriciaLab7
+﻿using System;
+using CiurdareanPatriciaLab7.Data;
+using System.IO;
+
+namespace CiurdareanPatriciaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
